6b32ff64f0376dcf11cda9db41028efd33f8b752
hilalsharifli <hilal@sharifli.com>
2020-02-26 20:20:04 +0400

    #1

---
+++ src/main/java/lesson10_3/charCount.java
@@ -0,0 +1,28 @@
+package lesson10_3;
+
+import java.util.*;
+import java.util.Set;
+
+public class charCount {
+   String charCounterMethod(String text) {
+
+        Set<String> textchars;
+        textchars = new HashSet<>();
+       for (int i = 0; i < text.length(); i++) {
+            textchars.add("" + text.charAt(i));
+       }
+       System.out.println(textchars);
+        StringBuilder str = new StringBuilder();
+        String result;
+
+        for (String i : textchars) {
+            int counter = 0;
+            for (int j = 0; j < text.length(); j++) {
+                if (i.equals(""+ text.charAt(j))) counter++;
+            }
+            str.append(i + ":" + counter + "\n");
+        }
+        return result = "" + str;
+    }
+}
+
---
+++ src/main/java/lesson10_3/charCountApp.java
@@ -0,0 +1,9 @@
+package lesson10_3;
+
+public class charCountApp {
+    public static void main(String[] args) {
+        charCount counter = new charCount();
+        System.out.println(counter.charCounterMethod("asaqqqdddfq"));
+    }
+
+}
